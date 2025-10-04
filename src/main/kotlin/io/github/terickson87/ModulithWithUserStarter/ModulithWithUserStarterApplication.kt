268f132a9382df8372b07a58c7558278d4a5bd00
Todd Ericskon <todd.erickson87@gmail.com>
2025-10-04 14:43:56 -0700

    added import from start.spring.io

---
+++ src/main/kotlin/io/github/terickson87/ModulithWithUserStarter/ModulithWithUserStarterApplication.kt
@@ -0,0 +1,11 @@
+package io.github.terickson87.ModulithWithUserStarter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModulithWithUserStarterApplication
+
+fun main(args: Array<String>) {
+	runApplication<ModulithWithUserStarterApplication>(*args)
+}
---
+++ src/test/kotlin/io/github/terickson87/ModulithWithUserStarter/ModulithWithUserStarterApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.github.terickson87.ModulithWithUserStarter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModulithWithUserStarterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
